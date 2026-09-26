a5fd8dc747c1dc692d2ea8464c7c9f60ff5c38d1
avamgir-wq <avamg.ir@gmail.com>
2026-09-26 14:48:46 +0330

    Add unit data model for apartments

---
+++ app/src/main/java/com/ava/hamyarsakhteman/Unit.java
@@ -0,0 +1,15 @@
+package com.ava.hamyarsakhteman;
+
+public class Unit {
+    public int number;
+    public String ownerName="";
+    public String phone="";
+    public String tenant="";
+    public String parking="";
+    public String storage="";
+    public boolean hasWaterMeter=false;
+
+    public Unit(int number){
+        this.number=number;
+    }
+}
