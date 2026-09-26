00006e64ea0df662c37716d8f835e498d185e97e
avamgir-wq <avamg.ir@gmail.com>
2026-09-26 14:55:48 +0330

    Extend reports with monthly summary

--- app/src/main/java/com/ava/hamyarsakhteman/ReportManager.java
+++ app/src/main/java/com/ava/hamyarsakhteman/ReportManager.java
@@ -5,6 +5,14 @@ public class ReportManager {
         return "گزارش واحد " + unit + "\nشارژ پرداختی:\nقبض آب:\nقبض برق:\nمانده بدهی:";
     }
 
+    public String monthlySummary(String month){
+        return "گزارش ماه " + month + "\n"+
+                "جمع شارژ دریافتی:\n"+
+                "قبوض آب و برق عمومی:\n"+
+                "هزینه های ساختمان:\n"+
+                "مانده صندوق:";
+    }
+
     public String smsText(int unit, String owner){
         return "مالک محترم واحد " + unit + " " + owner + "\nیادآوری پرداخت شارژ و قبوض ساختمان";
     }
